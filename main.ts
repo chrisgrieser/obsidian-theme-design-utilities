b72d0eaa6ae7305a6ac3463e42ca0e4e7cb12f8f
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2021-12-05 03:47:54 +0100

    add freeze command

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { Notice, Plugin } from "obsidian";
 
 declare module "obsidian" {
-	// to add type safety for the undocumented methods
+	// add type safety for the undocumented methods
 	interface App {
 		emulateMobile: (toggle: boolean) => void;
 		isMobile: () => void;
@@ -11,6 +11,8 @@ declare module "obsidian" {
 export default class themeDesignUtilities extends Plugin {
 
 	async onload() {
+
+		const freezeDelaySecs = 5;
 
 		this.addCommand({
 			id: "test-notice",
@@ -24,6 +26,15 @@ export default class themeDesignUtilities extends Plugin {
 			callback: () => {
 				if (this.app.isMobile) this.app.emulateMobile(false);
 				else this.app.emulateMobile(true);
+			},
+		});
+
+		this.addCommand({
+			id: "freeze-obsidian",
+			name: "Freeze Obsidian (triggered with " + freezeDelaySecs.toString() + "s delay)",
+			callback: () => {
+				new Notice ("Will freeze Obsidian in " + freezeDelaySecs.toString() + "s (if the console is open.)", 3000); // eslint-disable-line no-magic-numbers
+				setTimeout(() => {debugger}, freezeDelaySecs * 1000); // eslint-disable-line no-magic-numbers
 			},
 		});
 
