c0c3fa2a0b18ed0c8bd4dbf85b1bd0e8ac7c334e
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2021-12-09 21:32:41 +0100

    better changelog

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { Notice, Plugin } from "obsidian";
 
 declare module "obsidian" {
 	// add type safety for the undocumented methods
-	interface App {
+	interface App { // eslint-disable-line no-shadow
 		emulateMobile: (toggle: boolean) => void;
 		isMobile: () => void;
 	}
@@ -12,19 +12,8 @@ export default class themeDesignUtilities extends Plugin {
 
 	async onload() {
 
-		const freezeDelaySecs = 5;
-
-		this.addCommand({
-			id: "test-notice",
-			name: "Test Notice",
-			callback: () => new Notice ("I am a test notice. 👋 \n\nI will stay here until you click me. ", 0),
-		});
-
-		this.addCommand({
-			id: "toggle-emulate-mobile",
-			name: "Toggle mobile emulation",
-			callback: () => this.app.emulateMobile(!this.app.isMobile),
-		});
+		const freezeDelaySecs = 4;
+		console.log("Theme Design Utilities Plugin loaded.");
 
 		this.addCommand({
 			id: "freeze-obsidian",
@@ -36,15 +25,45 @@ export default class themeDesignUtilities extends Plugin {
 		});
 
 		this.addCommand({
+			id: "test-notice",
+			name: "Test Notice",
+			callback: () => new Notice ("I am a test notice. 👋 \n\nI will stay here until you click me.", 0),
+		});
+
+		this.addCommand({
+			id: "toggle-emulate-mobile",
+			name: "Toggle mobile emulation",
+			callback: () => this.app.emulateMobile(!this.app.isMobile),
+		});
+
+		this.addCommand({
 			id: "cheatsheet-css-classes",
 			name: "Cheatsheet: Obsidian CSS Classes",
 			callback: () => window.open("https://raw.githubusercontent.com/chrisgrieser/obsidian-theme-design-utilities/master/cheatsheets/css-classes.png"),
 		});
 
-		console.log("Theme Design Utilities Plugin loaded.");
+		this.addCommand({
+			id: "toggle-dark-light-mode",
+			name: "Toggle Dark/Light Mode",
+			callback: () => {
+				const isDarkMode = this.app.vault.getConfig("theme") === "obsidian";
+				if (isDarkMode) this.useLightMode();
+				else this.useDarkMode();
+			}
+		});
 	}
 
-	async onunload() {
-		console.log("Theme Design Utilities Plugin unloaded.");
+	async onunload() { console.log("Theme Design Utilities Plugin unloaded.") }
+
+	useDarkMode() {
+		this.app.setTheme("obsidian");
+		this.app.vault.setConfig("theme", "obsidian");
+		this.app.workspace.trigger("css-change");
+	}
+
+	useLightMode() {
+		this.app.setTheme("moonstone");
+		this.app.vault.setConfig("theme", "moonstone");
+		this.app.workspace.trigger("css-change");
 	}
 }
