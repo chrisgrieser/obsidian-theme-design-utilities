6e2553190a40d5be847e206ac5347242d0b1a2e3
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-01-05 11:07:27 +0100

    minor

--- .release.sh
+++ .release.sh
@@ -47,6 +47,8 @@ git log --pretty=format:"- %ad%x09%s" --date=short | egrep "\t\+|\trelease \d" >
 # push the manifest and versions JSONs
 git add -A
 git commit -m "release $nextVersion"
+
+git pull
 git push
 
 # trigger the release action
